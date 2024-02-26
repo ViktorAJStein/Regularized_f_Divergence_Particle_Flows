867ffcf0e0085fb962ec67176e295c5b50b477d0
Viktor Stein <103535157+ViktorAJStein@users.noreply.github.com>
2024-02-26 19:30:45 +0100

    added recession constant, removed hessians

--- entropies.py
+++ entropies.py
@@ -11,20 +11,9 @@ def tsallis_generator(x, alpha):
 
 def tsallis_generator_der(x, alpha):
     return np.choose(x >= 0, [np.inf, alpha / (alpha - 1) * ( (x+1e-30)**(alpha - 1) - 1)])
-    
-def kl_generator_hess(x):
-    return np.choose(x > 0, [np.inf, 1/x])
 
-def tsallis_generator_hess(x, alpha):
-    return np.choose(x >= 0, [np.inf, alpha * (x+1e-30)**(alpha - 2)])
-    
-def tsallis_hess(x, alpha):
-    if alpha != 1:
-        return tsallis_generator_hess(x, alpha)
-    else:
-        return kl_generator_hess(x)
 
-# the conjugate f_alpha* of the entropy function f_alpha
+# the conjugate f_alpha* of the entropy function f_alpha for alpha > 1
 def tsallis_conj(x, alpha):
     if alpha != 1:
         return reLU((alpha - 1)/alpha * x + 1)**(alpha / (alpha - 1)) - 1
@@ -115,3 +104,46 @@ def tv_conj(y, alpha):
 
 def tv_conj_der(x, alpha):
     return np.select([np.abs(x) <= 1], [1], default=0)
+    
+    
+# define recession constants
+def rec_const(div, alpha = None):
+    if div == 'power':
+        if alpha >= 1:
+            return float('inf')
+        elif alpha < 1 and alpha != 0:
+            return 1 / (1 - alpha)
+        elif alpha == 0:
+            return 1
+
+    if div == 'tsallis':
+        if alpha >= 1:
+            return float('inf')
+        elif 0 < alpha < 1:
+            return alpha / (1 - alpha)
+
+    if div in ['jeffreys', 'chi']:
+        return float('inf')
+     
+    if div == 'Lindsay':
+        return 1/(1 - alpha)
+        
+            
+    if div in ['tv', 'reverse_KL', 'matusita', 'kafka']
+        return 1
+    
+    if div == 'marton':
+        return 0
+        
+    if div == 'jensen_shannon':
+        return np.sqrt(2)
+        
+    if div == 'perimeter':
+        if alpha > 0:
+            return 1/(1 - alpha) * (1 - 2**(alpha - 1))
+        elif alpha < 0:
+            return 1/(1 - alpha) * 2**(alpha - 1)
+        elif alpha == 0:
+            return 1/2
+        elif alpha == 1:
+            return np.log(2)
